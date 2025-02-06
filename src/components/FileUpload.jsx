224cb5c01b6269d32d4f86693a04e212318433ad
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2025-02-06 11:34:16 +0800

    feat: file upload and image preview done.

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { UTF8_GENERAL_MYSQL500_CI } from "mysql/lib/protocol/constants/charsets";
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
+  const [image, setImage] = useState(null)
 
   // Handle file selection
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    console.log('handling file upload')
+    const selectedFile = e.target.files[0]
+    setFile(selectedFile);
+
+    if (selectedFile) {
+      console.log('setting preview image')
+      const reader = new FileReader();
+      const blob = new Blob([selectedFile], { type: selectedFile.type })
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    }
   };
 
   // Handle file upload
@@ -14,27 +28,30 @@ const FileUpload = () => {
     if (!file) {
       alert("Please select a file first!");
       return;
+    } else {
+      alert('File ready to upload!')
     }
 
-    const formData = new FormData();
-    formData.append("file", file); // "file" is the key your backend will expect
+    // const formData = new FormData();
+    // formData.append("file", file); // "file" is the key your backend will expect
 
-    try {
-      const response = await axios.post("http://localhost:5000/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data", // Set correct content type
-        },
-      });
-      console.log("File uploaded successfully:", response.data);
-    } catch (error) {
-      console.error("Error uploading file:", error);
-    }
+    // try {
+    //   const response = await axios.post("http://localhost:5000/upload", formData, {
+    //     headers: {
+    //       "Content-Type": "multipart/form-data", // Set correct content type
+    //     },
+    //   });
+    //   console.log("File uploaded successfully:", response.data);
+    // } catch (error) {
+    //   console.error("Error uploading file:", error);
+    // }
   };
 
   return (
     <div className="p-4">
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} className="ml-2 p-2 bg-blue-500 text-white rounded">
+      {image && <img src={image} alt="Preview"/>}
+      <button onClick={handleUpload}>
         Upload
       </button>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FileUpload from './components/FileUpload'
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <FileUpload />
   </React.StrictMode>,
 )
