78c7564adcbdc588c6de836921995e5a3f5b6642
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2025-02-17 10:55:03 +0800

    feat: created camera component in components/Camera.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FileUpload from './components/FileUpload'
+import Camera from './components/Camera'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Camera />
     <FileUpload />
   </React.StrictMode>,
 )
