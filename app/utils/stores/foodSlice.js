c43f6c0c1d1e6389a218551cb9978928dfd16bc0
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2025-02-26 17:39:56 +0800

    feat: created redux store for foods.

---
+++ app/utils/stores/foodSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const foodSlice = createSlice({
+    name: 'food',
+    initialState: {
+        foodImg: null,
+        foodName: null,
+        servingSize: null,
+        calories: null,
+        carbohydrate: null,
+        protein: null,
+        fat: null,
+        fiber: null,
+        sugar: null,
+        sodium: null,
+        cholesterol: null,
+        saturated_fat: null
+    },
+    reducers: {
+        setFood: (state, action) => {
+            state.foodName = action.payload['Food Name']
+            state.servingSize = action.payload['Serving Size']
+            state.calories = action.payload['Calories']
+            state.carbohydrate = action.payload['Total Carbohydrates']
+            state.protein = action.payload['Protein']
+            state.fat = action.payload['Total Fat']
+            state.fiber = action.payload['Dietary Fiber']
+            state.sugar = action.payload['Sugars']
+            state.sodium = action.payload['Sodium']
+            state.cholesterol = action.payload['Cholesterol']
+            state.saturated_fat = action.payload['Saturated Fat']
+        },
+        setFoodImg: (state, action) => {
+            state.foodImg = action.payload
+        }
+    }
+})
+
+export const { setFood, setFoodImg } = foodSlice.actions
+
+export default foodSlice.reducer
--- app/utils/stores/store.js
+++ app/utils/stores/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import foodReducer from "./foodSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    food: foodReducer,
   },
 });
 
