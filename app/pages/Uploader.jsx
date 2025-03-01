17b361b623baa5f5ebcbafefaf6d0783857fd05e
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2025-03-01 18:31:12 +0800

    fix: fixed variables to add food entry to supabase, deleted commented codes.

--- app/pages/Uploader.jsx
+++ app/pages/Uploader.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../utils/supabase"
-import FileUploader from "../components/FileUploader";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import AuthPrompt from "../components/AuthPrompt";
@@ -9,34 +8,6 @@ export default function Uploader() {
     const user = useSelector((state) => state.user.user)
     const food = useSelector((state) => state.food.food)
     const image = useSelector((state) => state.food.foodImg)
-    // const [file, setFile] = useState(null); // Store file object here
-    // const [image, setImage] = useState(null)
-    // const [foodName, setFoodName] = useState(null)
-    // const [servingSize, setServingSize] = useState(null)
-    // const [nutrition, setNutrition] = useState({
-    //   calories: null,
-    //   carbohydrate: null,
-    //   protein: null,
-    //   fat: null,
-    //   fiber: null,
-    //   sugar: null,
-    //   sodium: null,
-    //   cholesterol: null,
-    //   saturated_fat: null
-    // });
-
-    // When file is set, display preview image
-    // useEffect(() => {
-    //   if(file) {
-    //     console.log('Setting preview image.')
-    //     const reader = new FileReader();
-    //     const blob = new Blob([file], { type: file.type })
-    //     reader.onloadend = () => {
-    //       setImage(reader.result);
-    //     };
-    //     reader.readAsDataURL(blob);
-    //   }
-    // }, [file])
 
       const addFoodEntry = async() => {
         console.log('Adding food entry to: ', user.id)
@@ -45,17 +16,17 @@ export default function Uploader() {
           .insert([
             {
               user_id: user.id,
-              food_name: foodName,
-              serving_size: servingSize,
-              calories: nutrition.calories,
-              fat: nutrition.fat,
-              saturated_fat: nutrition.saturated_fat,
-              cholesterol: nutrition.cholesterol,
-              sodium: nutrition.sodium,
-              carbohydrate: nutrition.carbohydrate,
-              fiber: nutrition.fiber,
-              sugar: nutrition.sugar,
-              protein: nutrition.protein,
+              food_name: food['Food Name'],
+              serving_size: food['Serving Size'],
+              calories: food['Calories'],
+              fat: food['Total Fat'],
+              saturated_fat: food['Saturated Fat'],
+              cholesterol: food['Cholesterol'],
+              sodium: food['Sodium'],
+              carbohydrate: food['Total Carbohydrates'],
+              fiber: food['Dietary Fiber'],
+              sugar: food['Sugars'],
+              protein: food['Protein'],
             }
           ]);
 
